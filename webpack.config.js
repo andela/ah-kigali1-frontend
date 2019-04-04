958899c422088755dc7d48d36f934bf0f915ae9c
Jean luc Abayo <luc@Jeans-MacBook-Pro.local>
2019-04-04 13:10:19 +0200

    chore(add): sass-loader processor
    - Add sass processor: style-loader, css-loader, and sass-loader
    - Add css extract plugin
    
    [Finishes #163518640]

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,50 @@
-const path = require("path");
-const HtmlPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index_bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.(js)$/,
-        exclude: "/node_modules/",
-        use: ["babel-loader"]
+        exclude: '/node_modules/',
+        use: ['babel-loader']
       },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] }
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      }
     ]
   },
-  mode: "development",
-  plugins: [new HtmlPackPlugin({ template: "./public/index.html" })]
+  mode: 'development',
+  plugins: [
+    new HtmlPackPlugin({ template: './public/index.html' }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css'
+    })
+  ]
 };
