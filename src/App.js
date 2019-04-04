2f10bb2263c50ce4f25ce37c08d6d9dd7fa0cb86
YvesIraguha <iraguhaivos@gmail.com>
2019-04-04 16:17:34 +0200

    chore(create):create eslint file
    -enable hound ci
    [starts #165054398]
    
    chore(fix): ignore unnecessary files
    
    chore(fix): remove unneccessary files
    
    [#164364254]Add search results page
    
    [#164364254]Add nav-bar to search page
    
    [#164364254]Remove broken tabs
    
    feat(user setting):Add user settings pages- Update password- Mute or unmute email notification- Delete my account
    
    [#164203446] add landing page -  desktop view
    
    [#164203446] add landing page -  responsive complete
    
    [feature #164395296] refactor sass
    
    [ch #[163518639]] create route for the application
    
    [ch #[163518639]] fix issue where link return 404
    
    [ft #[163518639]] remove navigation bar
    
    [ch #[163518639]] refractor the code by putting routes in App.js
    
    [ch #[163518639]]  update .gitignore file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
-import { Provider } from "react-redux";
-import "./styles/main.scss";
-import store from "./redux/store";
+import React, { Component } from "react";
+import {
+  Route,
+  Switch,
+  BrowserRouter as Router,
+  Redirect
+} from "react-router-dom";
+import Home from "./views/home";
+import NotFound from "./views/Not-Found";
 import Login from "./views/Login";
 
-export default () => (
-  <Provider store={store}>
-    <Login />
-  </Provider>
-);
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/">
+              <Redirect to="/home" />
+            </Route>
+            <Route path="/login" component={Login} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
