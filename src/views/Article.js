eabb4cf0bdf9c8e20a2b49ca8f2707686accda88
YvesIraguha <iraguhaivos@gmail.com>
2019-04-05 22:13:37 +0200

    chore(fix): fix broken code

--- src/views/Article.js
+++ src/views/Article.js
@@ -1,38 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import addArticle from "../redux/actions/index";
 import "../index.css";
 
-const mapDispatchToProps = dispatch => {
-  return {
-    addArticle: () => dispatch(addArticle())
-  };
-};
+const mapDispatchToProps = dispatch => ({
+  addArticle: () => dispatch(addArticle())
+});
 
-class FetchedArticles extends Component {
-  constructor() {
-    super();
-    this.handleFetch = this.handleFetch.bind(this);
-  }
+const FetchedArticles = props => (
+  <div>
+    <h1>Fetch some articles from dummy app</h1>
 
-  handleFetch() {
-    // eslint-disable-next-line react/destructuring-assignment
-    this.props.addArticle();
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Fetch some articles from dummy app</h1>
-
-        <button onClick={this.handleFetch} className="btn" type="submit">
-          Fetch more articles
-        </button>
-      </div>
-    );
-  }
-}
+    <button
+      // eslint-disable-next-line react/destructuring-assignment
+      onClick={() => props.addArticle()}
+      className="btn"
+      type="submit"
+    >
+      Fetch more articles
+    </button>
+  </div>
+);
 
 FetchedArticles.propTypes = {
   // eslint-disable-next-line react/require-default-props
