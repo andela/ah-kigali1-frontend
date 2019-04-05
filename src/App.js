79734e61010c9827d281ac81815ec65da8bc4098
YvesIraguha <iraguhaivos@gmail.com>
2019-04-05 22:49:28 +0200

    chore(fix): fix conflicts

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import store from "./redux/store";
 import Home from "./views/Home";
 import Article from "./views/Article";
 
-window.store = store;
-
 export default () => (
   <Provider store={store}>
     <Article />
