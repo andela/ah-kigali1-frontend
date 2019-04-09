e1d270982a5ed068b86c610a34309519d9cb56bb
espoirMur <espoir.mur@gmail.com>
2019-04-09 14:34:02 +0200

    [ch #[163518639]] fix rebase conflict from develop

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import store from "./redux/store";
+import "./style/main.scss";
 
 ReactDOM.render(<App />, document.getElementById("app"));
