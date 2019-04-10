f2bf449bafdfdce50ab09034735b639490f5f444
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2019-04-10 05:04:49 +0200

    init commit#163518646

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import loginReducers from "./loginReducers";
+import sampleReducers from "./sampleReducers";
+import userReducer from "./userReducer";
 
 export default combineReducers({
+  sample: sampleReducers,
+  user: userReducer,
   login: loginReducers
 });
---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,45 @@
+import {
+  SET_PROFILE,
+  SET_LOADING,
+  SET_ERROR,
+  SET_FORM_INPUT,
+  SET_SUCCESS
+} from "../actionTypes";
+const initialState = {
+  profile: {
+    email: "",
+    username: "",
+    address: null,
+    allowNotifications: true,
+    bio: "",
+    firstName: "",
+    gender: "",
+    image: "",
+    lastName: "",
+    password: "",
+    phone: "",
+    socialId: ""
+  },
+  loading: false,
+  error: null
+};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case SET_PROFILE:
+      return { ...state, profile: payload };
+    case SET_LOADING:
+      return { ...state, loading: payload };
+    case SET_FORM_INPUT:
+      return {
+        ...state,
+        profile: { ...state.profile, [payload.field]: payload.value }
+      };
+    case SET_ERROR:
+      return { ...state, error: payload };
+    case SET_SUCCESS:
+      return { ...state, message: payload };
+    default:
+      return state;
+  }
+};
