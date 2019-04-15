31f3c9918fc3c1a5a98707a319db2cc796965c97
Luc <luc.bayo@gmail.com>
2019-04-15 16:03:16 +0200

    [#163518643]Add page for updating password

--- src/redux/actions/updatePassword.js
+++
@@ -1,6 +0,0 @@
-import { UPDATE_PASSWORD_INPUT_CHANGE } from "../actionTypes";
-
-export const handleInputChange = (field, value) => ({
-  type: UPDATE_PASSWORD_INPUT_CHANGE,
-  payload: { field, value }
-});
