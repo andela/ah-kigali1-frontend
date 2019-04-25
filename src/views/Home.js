7221749a0ea67af12cf7150b9299ad7e11dee089
Luc <luc.bayo@gmail.com>
2019-04-25 11:32:05 +0200

    [Feature #163518658] article search & filter
    - Convert HTML & CSS mock into React component
    - Fetchs article and store them into Redux state
    - Displies fetchec article
    - Test: component, reducers, and action creators
    - Refactors app routing
    
    [Finishes #163518658]

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,50 @@
 import React, { Component } from "react";
+import BasicButton from "../components/common/Buttons/BasicButton";
+import Input from "../components/common/Inputs/TextInput";
+import MainCard from "../components/common/Cards/main";
+import CategoryBar from "../components/common/AppBars/CategoryBar";
+import Latest from "../components/common/Cards/latest";
+
+const cats = [
+  "TECH",
+  "CULTURE",
+  "FILM",
+  "FASHION",
+  "POLITICS",
+  "FITNESS",
+  "SCIENCE",
+  "BUSINESS",
+  "TRAVEL",
+  "LEISURE",
+  "FOOD"
+];
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <div>Home Page</div>
+        <CategoryBar catList={cats} onMoreClick={() => {}} onClick={() => {}} />
+        <div style={{ margin: 3, marginTop: 120 }}>
+          <h2 className="home-container">Hello world, from Titan-Devs</h2>
+          <BasicButton style={{ width: "40%" }} title="Save" />
+          <Input
+            style={{ width: "40%" }}
+            type="email"
+            name="email"
+            placeholder="Your email"
+          />
+          <Input
+            style={{ width: "60%" }}
+            type="password"
+            name="email"
+            placeholder="Enter your password"
+          />
+        </div>
+        <div className="featured_article">
+          <MainCard />
+          <Latest />
+        </div>
+        <Latest />
       </div>
     );
   }
