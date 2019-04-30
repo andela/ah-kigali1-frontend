a916e8b64d223f75cb552a0e360cc2fb9637e683
Luc <luc.bayo@gmail.com>
2019-04-30 12:38:16 +0200

    [Feature #163518658] add loading animation and error message
    - Designs loading animation with SCSSS
    - Creates loading components
    - Display loading animation or errors message on SearchResults page

---
+++ src/components/Animations/Loading.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const BottomPopUp = () => (
+  <div className="lds-css ng-scope loading">
+    <div style={{ width: "100%", height: "100%" }} className="lds-ellipsis">
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+      <div>
+        <div />
+      </div>
+    </div>
+  </div>
+);
+
+export default BottomPopUp;
