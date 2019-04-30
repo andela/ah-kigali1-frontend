aa8bb9573dceb7fbd828a2fb10f7a7b768e0d3b0
YvesIraguha <iraguhaivos@gmail.com>
2019-04-30 13:28:22 +0200

    feature(fix): fix tests

--- src/__tests__/__utils__/helperFunctions.test.js
+++ src/__tests__/__utils__/helperFunctions.test.js
@@ -16,6 +16,7 @@ describe("Helper Functions", () => {
       expect(isEmpty(null)).toBe(true);
     });
   });
+
   describe("capitalize", () => {
     it("should capitalize the first letter", () => {
       expect(capitalize("luc")).toBe("Luc");
@@ -35,18 +36,21 @@ describe("Helper Functions", () => {
       );
     });
   });
+
   describe("time stamp calculation", () => {
     test("should return the right format of the data", () => {
       const time = "2019-04-17T08:06:06.002Z";
       expect(calculateTimeStamp(time)).toEqual("Wed Apr 17 2019");
     });
   });
+
   describe("checking current user ", () => {
     test("should return true when current user is the same as author", () => {
       expect(isCurrentUserAuthor("Iraguha1", { username: "Iraguha1" })).toEqual(
         true
       );
     });
+
     test("should return false when current user is different from author", () => {
       expect(isCurrentUserAuthor("Iraguha1", { username: "yves" })).toEqual(
         false
