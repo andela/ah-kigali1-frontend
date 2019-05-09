57bd731f8bf530163ef8638a94f87c996be537df
YvesIraguha <iraguhaivos@gmail.com>
2019-05-09 17:47:11 +0200

    feature(fix): fix empty lines on end of tests

--- src/__tests__/__components__/Link/Social.test.js
+++ src/__tests__/__components__/Link/Social.test.js
@@ -4,8 +4,8 @@ import { shallow } from "enzyme";
 import Social from "../../../components/common/Link/Social";
 
 const props = {
-  image: "../../../components/common/Link/Social",
-  href: "../../../components/common/Link/Social"
+  image: "fakeImage",
+  href: "fakeLink"
 };
 
 describe("Social button", () => {
@@ -13,9 +13,11 @@ describe("Social button", () => {
   beforeEach(() => {
     component = shallow(<Social {...props} />);
   });
+
   test("should match the snapshot", () => {
     expect(toJson(component)).toMatchSnapshot();
   });
+
   test("should render button", () => {
     window.open = jest.fn();
     component.simulate("click");
